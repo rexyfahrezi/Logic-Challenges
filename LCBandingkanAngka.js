692aa672e24c7f0e8d6bcecb1509cf342d344d0a
Rexy <rexyfahrezi30@gmail.com>
2018-10-04 11:15:44 +0700

    Logic-Challenges

--- LCBandingkanAngka.js
+++ LCBandingkanAngka.js
@@ -1,8 +1,14 @@
 function bandingkanAngka(angka1,angka2){
 	if (angka1 === angka2){
-	 return -1 //output return 1
+		return -1 //output return 1
 	}
-	return angka1 < angka2 //output true
-} //output false
-
-console.log(bandingkanAngka(6,5))
+	if (angka1 < angka2){
+		return true //output true
+	}
+	if (angka1 > angka2){
+		return false //output false
+	}
+}
+console.log(bandingkanAngka(6,5)) // false
+console.log(bandingkanAngka(2,2)) // -1
+console.log(bandingkanAngka(5,6)) // true
